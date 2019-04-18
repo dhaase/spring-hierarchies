d14383c0c10770610124989831d20d30e343e3e8
dhaase <dhaase@users.noreply.github.com>
2019-04-18 21:55:14 +0200

    intermediate commit

--- src/main/java/eu/dirk/haase/Main.java
+++ src/main/java/eu/dirk/haase/Main.java
@@ -1,11 +1,16 @@
 package eu.dirk.haase;
 
-import eu.dirk.haase.bean.MainServiceOne;
-import eu.dirk.haase.bean.MainServiceTwo;
 import eu.dirk.haase.bean.ServiceTwo;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.AutowireCandidateQualifier;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Arrays;
+import java.lang.annotation.Annotation;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
@@ -27,6 +32,9 @@ public class Main {
         ctx_all_domains.setConfigLocations(resources2);
         ctx_all_domains.refresh();
 
+        String value = getBean(ctx_all_domains, String.class, MyQualifier.class, "one1");
+        System.out.println(value);
+
         String[] resources3 = {"/eu/dirk/haase/application-context-main.xml"};
         ClassPathXmlApplicationContext ctx_main = new ClassPathXmlApplicationContext();
         ctx_main.setId("ctx_main");
@@ -37,6 +45,34 @@ public class Main {
 
         ServiceTwo serviceTwo = ctx_main.getBean("one.serviceTwo", ServiceTwo.class);
         serviceTwo.execute();
+    }
+
+
+    public static <T> T getBean(final ConfigurableApplicationContext context,
+                                final Class<T> typeClass,
+                                final Class<? extends Annotation> qualifierClass,
+                                final String attributeValue) throws NoSuchBeanDefinitionException {
+        final Map<String, T> beanMap = context.getBeansOfType(typeClass);
+        final ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+
+        for (final String beanName : beanMap.keySet()) {
+            if (beanFactory.containsBeanDefinition(beanName)) {
+                final BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+                if (beanDefinition instanceof AbstractBeanDefinition) {
+                    final AbstractBeanDefinition abstractBeanDefinition = (AbstractBeanDefinition) beanDefinition;
+                    final AutowireCandidateQualifier autowireCandidateQualifier = abstractBeanDefinition.getQualifier(qualifierClass.getName());
+                    if (autowireCandidateQualifier != null) {
+                        final Object value = autowireCandidateQualifier.getAttribute("value");
+                        if (attributeValue.equals(value)) {
+                            return beanMap.get(beanName);
+                        }
+                    }
+                }
+            }
+        }
+
+        final String qualifier = "With Qualifier " + qualifierClass.getName() + "('" + attributeValue + "')";
+        throw new NoSuchBeanDefinitionException(typeClass, qualifier);
     }
 
 
